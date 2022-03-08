4dc8708a081da945b36644e1b4e70e4c9203153c
jigna12 <kgotgb@gmail.com>
2022-03-08 23:28:15 +0000

    harikrishna01

---
+++ kgpractice01.java
@@ -0,0 +1,14 @@
+package harikrishna01;
+
+public class kgpractice01 {
+    public static void main (String [] args){
+        System.out.println("shree hari");
+        System.out.println("hair");
+        System.out.println("sima patel");
+        System.out.print("visastatus");
+        System.out.println("veer");
+
+
+    }
+
+}
---
+++ kgpractice02.java
@@ -0,0 +1,14 @@
+package harikrishna01;
+
+public class kgpractice02 {
+    public static void main (String [] args){
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b-a);
+    }
+}
